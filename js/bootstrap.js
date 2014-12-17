7634e4a8b1456491bc44afe35c597e3b3e7b14d8
Harry Parton <hparton@hotmail.co.uk>
2014-12-17 15:35:16 +0000

    Update Images, Add new font for everything and responsify!

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,17 +1,53 @@
 
 $(document).ready(function(){
-	$('.js-slideshow').slick({
-	     focusOnSelect: true,
-	     slidesToShow: 1,
-	     nextArrow: '#right',
-	     prevArrow: '#left',
-	     cssEase: 'cubic-bezier(0.770, 0.000, 0.175, 1.000)',
-	     speed: 900
-	});
+
+	if ($(window).innerWidth() > 600 ) {
+		$('.js-slideshow').slick({
+		     focusOnSelect: true,
+		     slidesToShow: 1,
+		     nextArrow: '#right',
+		     prevArrow: '#left',
+		     cssEase: 'cubic-bezier(0.770, 0.000, 0.175, 1.000)',
+		     speed: 900,
+		     autoplay: true,
+		     autoplaySpeed: 10000,
+		});
+	} else {
+		$('.js-slideshow').slick({
+		     slidesToShow: 1,
+		     nextArrow: '#right',
+		     prevArrow: '#left',
+		     cssEase: 'ease',
+		     speed: 300,
+		     autoplay: true,
+		     autoplaySpeed: 5000,
+		});
+	}
+
+var allColumns = '';
+
+allColumns = true;
+
+
 
 	$('.left-hand-reveal').on('click',function(){
 		$('.yotm-wrapper').toggleClass('open');
 		$('.shop-info').toggleClass('off-screen');
+		if ($(window).innerWidth() < 900 ) {
+		    if (allColumns) {
+		        setTimeout(function(){
+					$('.yotm-wrapper').toggleClass('scrollable');
+					$('body').toggleClass('noscroll');
+				},740);
+		        allColumns = false;
+		    } else {
+		        setTimeout(function(){
+					$('.yotm-wrapper').toggleClass('scrollable');
+					$('body').toggleClass('noscroll');
+				},400);
+		        allColumns = true;
+		    }
+		}
 	});
 
 });
